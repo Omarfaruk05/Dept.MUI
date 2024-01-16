ab62d7ef0e48ed867c2b66d25134df066e6bb66c
unknown <mdomarfaruk149518@gmail.com>
2024-01-16 23:44:32 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import "./App.css";
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+  return (
+    <div
+      style={{
+        maxWidth: "1280px",
+        margin: "0 auto",
+      }}
+    >
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,73 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { FormGroup, TextField, Button } from "@mui/material";
+import loginPic from "../assets/20824344_6343825.jpg";
+
+interface IUserInfo {
+  name: string;
+  phoneNumber: string;
+  email: string;
+}
+
+const LoginPage = () => {
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const userInfo: IUserInfo = {
+      name: e.target.name.value,
+      phoneNumber: e.target.phoneNumber.value,
+      email: e.target.email.value,
+    };
+
+    await localStorage.setItem("userInfo", JSON.stringify(userInfo));
+  };
+  return (
+    <div
+      style={{
+        height: "94vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <img style={{ width: "50%" }} src={loginPic} alt="" />
+
+      <form
+        style={{
+          width: "400px",
+          textAlign: "center",
+        }}
+        onSubmit={handleSubmit}
+      >
+        <FormGroup>
+          <TextField
+            name="name"
+            label="Name"
+            variant="outlined"
+            margin="normal"
+          />
+          <TextField
+            name="phoneNumber"
+            label="Phone Number"
+            variant="outlined"
+            margin="normal"
+          />
+          <TextField
+            name="email"
+            label="Email"
+            variant="outlined"
+            margin="normal"
+          />
+        </FormGroup>
+        <Button
+          style={{ width: "100%", padding: "10px 0" }}
+          type="submit"
+          variant="contained"
+          color="info"
+        >
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+};
+export default LoginPage;
